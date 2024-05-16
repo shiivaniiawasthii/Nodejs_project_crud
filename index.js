674dde556418ace79f856d54714ce3f52d2d5d8e
shiivaniiawasthii <shvaniawsthi001@gmail.com>
2024-05-16 22:31:32 +0530

    Created server with routes in express js

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const fs = require("fs");
-const routeHandler = require("./routes/route");
 
-const server = http.createServer(routeHandler);
+const app = express();
 
-server.listen(3000);
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/message", (req, res, next) => {
+  console.log(req.body, "jhjhghghf");
+  console.log(req.body.message, "jhjhghghf");
+  const writemessage = req.body.message;
+  fs.writeFileSync("message.txt", writemessage);
+  res.statusCode = 302;
+  res.setHeader("location", "/");
+  res.end();
+});
+app.use("/", (req, res, next) => {
+  res.end(
+    `<html>
+<body>
+<form method="POST" action="/message">
+<input placeholder="Enter data" name="message"/>
+<button type="submit">Send</button>
+</form>
+</body>
+</html>`
+  );
+});
+
+app.listen(3000);
